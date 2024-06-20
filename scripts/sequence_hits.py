e6ae0ddfac2e312a7a83be040a4ccc16e5f56065
zeeboe <zansari@utexas.edu>
2024-06-20 18:50:54 -0500

    Update sequence_hits.py
    added the shebang line on the top

--- scripts/sequence_hits.py
+++ scripts/sequence_hits.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 # ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ 
 # ♡        Finding sequentially similar hits        ♡ 
 # ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡  
