cca215f63b478e0fd7066050ac40513bda493d12
zeeboe <zansari@utexas.edu>
2024-06-25 18:43:15 -0500

    Update and rename sequential_hits_commands.sh to commands.sh
    final for sequences

---
+++ notebooks/commands.sh
@@ -0,0 +1,25 @@
+# ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ 
+# ♡        Finding sequentially similar hits        ♡ 
+# ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡  
+
+# this script will generate a '{query}_sequential_processed_hits.csv' file that is populated by sequentially similar hits and can be easily read as a dataframe
+# OPTIONAL: Filter the resulting data frame before extracting sequences of interest
+
+python3 sequence_hits.py query path/to/scripts /path/to/home database.fasta
+
+
+# ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ 
+# ♡            Attaching sequences to hits          ♡ 
+# ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ 
+
+# this script will generate a '{query}_hits_sequences.fasta' file that is populated by sequences of sequentially similar hits to the query
+
+# run the following command in the terminal
+python3 path/to/scripts/extract_sequences.py query /path/to/home /path/to/database.fasta
+
+
+# ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ 
+# ♡        Finding structurally similar hits        ♡ 
+# ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ 
+
+# IN PROGRESS BUT ON ITS WAY ᕕ(◕ヮ◕)ᕗ
